f8fb3550b8c88cb5740bc1a1992305d101e761f6
CharlesPereira <charlesti.ptu@gmail.com>
2024-01-28 17:50:45 -0300

    feat: create getters and setters in the Entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,11 @@
+import dayjs from 'dayjs'
 import { Entity } from '../../core/entities/entity'
 import { UniqueEntityID } from '../../core/entities/unique-entity-id'
 import { Optional } from '../../core/types/option'
 import { Slug } from './values-objects/slug'
 
 interface QuestionProps {
-  authoId: UniqueEntityID
+  authorId: UniqueEntityID
   bestAnswerId?: UniqueEntityID
   title: string
   content: string
@@ -14,9 +15,62 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
-  static create(props: Optional<QuestionProps, 'createdAt'>, id?: UniqueEntityID) {
+  get authorId() {
+    return this.props.authorId
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updateAt() {
+    return this.props.updateAt
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+  }
+
+  get except() {
+    return this.content.substring(0.120).trimEnd().concat('...')
+  }
+
+  private touch() {
+    this.props.updateAt = new Date()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
+  static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityID) {
     const question = new Question({
       ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
       createdAt: new Date()
     }, id)
 
