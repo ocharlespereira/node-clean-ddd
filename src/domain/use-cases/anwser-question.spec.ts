6ce67e268a829902ed67fc53864a8b262839d918
CharlesPereira <charlesti.ptu@gmail.com>
2024-01-28 15:47:15 -0300

    test: new repository AnswerRepository.create

--- src/domain/use-cases/anwser-question.spec.ts
+++ src/domain/use-cases/anwser-question.spec.ts
@@ -1,11 +1,19 @@
 import { expect, test } from 'vitest'
+import { Answer } from '../entities/answer'
+import { AnswersRepository } from '../repositories/answers-respository'
 import { AnwserQuestionUseCase } from './anwser-question'
 
+const fakeAnswersRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return
+  }
+}
 
-test('create an answer', () => {
-  const answerQuestion = new AnwserQuestionUseCase()
 
-  const answer = answerQuestion.execute({
+test('create an answer', async () => {
+  const answerQuestion = new AnwserQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
     content: 'New answer'
