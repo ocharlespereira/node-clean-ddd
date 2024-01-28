66806ca9f00aff5740ce889d49b43b03523a8caf
CharlesPereira <charlesti.ptu@gmail.com>
2024-01-28 16:05:32 -0300

    feat: create new Class Slug static return Slug value

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,19 +1,23 @@
 import { randomUUID } from 'node:crypto'
+import { Slug } from './values-objects/slug'
 
 interface QuestionProps {
   title: string
   content: string
+  slug: Slug
   authoId: string
 }
 
 export class Question {
   public id: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title
+    this.slug = props.slug
     this.content = props.content
     this.authorId = props.authoId
     this.id = id ?? randomUUID()
---
+++ src/domain/entities/values-objects/slug.ts
@@ -0,0 +1,32 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receives a string and normalize it as slug
+   * 
+   * Example: "An Exemple title" => "an-example-title"
+   * 
+   * 
+   * @param text {string}
+   * 
+   */
+
+  static createFromText(text: string) {
+    const slugText = text.normalize('NFKD') //remove qualquer tipo de acentuaçao da string
+      .toLowerCase() //caixa baixa
+      .trim() //retira espaçamento inicio e fim
+      .replace(/\s+/g, '-') //substitui os espaços em branco por traço
+      .replace(/[^\w-]+/g, '') // retira simbolos
+      .replace(/_/g, '-') // substitui _ por -
+      .replace(/--+/g, '-') // substitui dois hifens(--) por um (-)
+      .replace(/-$/g, '') // no final tiver hifem, substitui por nada
+
+
+
+    return new Slug(slugText)
+  }
+}
