56157d9c56b5aa2e60a6cd0af02c8f6d62559d4e
CharlesPereira <charlesti.ptu@gmail.com>
2024-01-28 16:05:38 -0300

    test: create new Class Slug static return Slug value

---
+++ src/domain/entities/values-objects/slug.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest';
+import { Slug } from './slug';
+
+test('it should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Exemple question title')
+
+  expect(slug.value).toEqual('exemple-question-title')
+})
