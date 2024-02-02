3d49760d9b365a0e7de96839190428489e915b42
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-01 21:27:56 -0300

    feat: create a DeleteAnswerCommentUseCase with make, delete and inmemory

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,12 +1,30 @@
-import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository"
-import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment"
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
