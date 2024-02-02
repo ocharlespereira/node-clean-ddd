fe2584b8a3d7474521c73c31c20b72f93128fb7e
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-01 21:23:07 -0300

    test: ensure delete another user question comment

--- src/domain/forum/application/use-cases/delete-question-comment.spec.ts
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { makeQuestionComment } from 'test/factories/make-question-comment'
 import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
 import { DeleteQuestionCommentUseCase } from './delete-question-comment'
@@ -23,5 +24,19 @@ describe('Delete Question Comment', () => {
     })
 
     expect(inMemoryQuestionCommentsRepository.items).toHaveLength(0)
+  })
+  it('should be able to delete another user question comment', async () => {
+    const questionComment = makeQuestionComment({
+      authorId: new UniqueEntityID('author-1'),
+    })
+
+    await inMemoryQuestionCommentsRepository.create(questionComment)
+
+    expect(() => {
+      return sut.execute({
+        questionCommentId: questionComment.id.toString(),
+        authorId: 'author-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
   })
 })
