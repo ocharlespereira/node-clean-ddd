458dd32851fc6b1e7f3e39e3f50074218c514bcd
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-02 22:55:38 -0300

    test: QuestionComments

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -0,0 +1,57 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestionComment } from 'test/factories/make-question-comment'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { FetchQuestionCommentsUseCase } from './fetch-question-comments'
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: FetchQuestionCommentsUseCase
+
+describe('Fetch Comments', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentsRepository)
+  })
+
+  it('should be able to fetch recent question commentss ', async () => {
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    )
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    )
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    )
+
+    const { questionComments } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(questionComments).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated question commentss', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionCommentsRepository.create(
+        makeQuestionComment({
+          questionId: new UniqueEntityID('question-1'),
+        })
+      )
+    }
+
+    const { questionComments } = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    })
+
+    expect(questionComments).toHaveLength(2)
+  })
+})
