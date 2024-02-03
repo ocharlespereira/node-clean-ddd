314b74755ad73afa40161a83f2b85302be7edf28
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-02 22:54:02 -0300

    feact: AnswerCommentsUsecase repository

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -12,6 +13,14 @@ export class InMemoryAnswerCommentsRepository
     if (!answerComment) {
       return null
     }
+
+    return answerComment
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComment = this.items
+      .filter((item) => answerId)
+      .slice((page - 1) * 20, page * 20)
 
     return answerComment
   }
