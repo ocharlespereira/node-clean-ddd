425bae095ee4c670a8866f6c344579c1ccef14d4
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-04 16:22:54 -0300

    refactor: apply refacture tuple success and error in the request and response

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,29 +1,30 @@
 import { Either, left, right } from '@/core/either'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/question-respository'
-import { QuestionNotFoundError } from './errors/question-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
 }
 
 type GetQuestionBySlugUseCaseResponse = Either<
-  QuestionNotFoundError,
+  NotAllowedError,
   {
     question: Question
   }
 >
 
 export class GetQuestionBySlugUseCase {
-  constructor(private questionrepository: QuestionsRepository) {}
+  constructor(private questionRepository: QuestionsRepository) {}
 
   async execute({
     slug,
   }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
-    const question = await this.questionrepository.findBySlug(slug)
+    const question = await this.questionRepository.findBySlug(slug)
 
     if (!question) {
-      return left(new QuestionNotFoundError())
+      // return left(new QuestionNotFoundError())
+      return left(new NotAllowedError())
     }
 
     return right({ question })
