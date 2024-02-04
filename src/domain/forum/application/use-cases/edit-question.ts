8f2a34e85acfb843d33563bd515d382702f808c5
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-04 18:12:35 -0300

    feat: edit list attachments used questionAttachmentsList

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -1,4 +1,8 @@
 import { Either, left, right } from '@/core/either'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { QuestionAttachement } from '../../enterprise/entities/question-attachment'
+import { QuestionAttachementList } from '../../enterprise/entities/question-attachment-list'
+import { QuestionAttachmentRepository } from '../repositories/question-attachments-respository'
 import { QuestionsRepository } from '../repositories/question-respository'
 import { NotAllowedError } from './errors/not-allowed-error'
 import { QuestionNotFoundError } from './errors/question-not-found-error'
@@ -8,6 +12,7 @@ interface EditQuestionseCaseRequest {
   title: string
   content: string
   questionId: string
+  attachmentsIds: string[]
 }
 
 type EditQuestionseCaseResponse = Either<
@@ -16,13 +21,17 @@ type EditQuestionseCaseResponse = Either<
 >
 
 export class EditQuestionseCase {
-  constructor(private questionRepository: QuestionsRepository) {}
+  constructor(
+    private questionRepository: QuestionsRepository,
+    private questionAttachmentRepository: QuestionAttachmentRepository
+  ) {}
 
   async execute({
     authorId,
     questionId,
     title,
     content,
+    attachmentsIds,
   }: EditQuestionseCaseRequest): Promise<EditQuestionseCaseResponse> {
     const question = await this.questionRepository.findById(questionId)
 
@@ -34,9 +43,32 @@ export class EditQuestionseCase {
       return left(new NotAllowedError())
     }
 
+    // busca todos os anexos que a pergunta ja tinha no BD
+    const currentQuestionAttachments =
+      await this.questionAttachmentRepository.findManyByQuestionId(questionId)
+
+    // cria uma lista passando os anexos q ja tem
+    const questionAttachmentList = new QuestionAttachementList(
+      currentQuestionAttachments
+    )
+
+    // cria uma nova lista de anexos apartir dos anexos
+    const questionAttachments = attachmentsIds.map((attachmentsIds) => {
+      return QuestionAttachement.create({
+        attachmentId: new UniqueEntityID(attachmentsIds),
+        questionId: question.id,
+      })
+    })
+
+    // agora compara as listas e verifica qm fica e qm sai da lista
+    questionAttachmentList.update(questionAttachments)
+
+    // add uma nova lista a question
     question.title = title
     question.content = content
+    question.attachments = questionAttachmentList
 
+    // e por fim salva a nova lista no BD
     await this.questionRepository.save(question)
 
     return right({})
---
+++ src/domain/forum/enterprise/entities/question-attachment-list.ts
@@ -0,0 +1,8 @@
+import { WatchedList } from '@/core/entities/watched-list'
+import { QuestionAttachement } from './question-attachment'
+
+export class QuestionAttachementList extends WatchedList<QuestionAttachement> {
+  compareItems(a: QuestionAttachement, b: QuestionAttachement): boolean {
+    return a.attachmentId === b.attachmentId
+  }
+}
