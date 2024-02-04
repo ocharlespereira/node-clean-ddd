6a815f5148396be5c0f29c6b30ae3ff46dd17d39
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-04 16:59:41 -0300

    feat: feat: create new entity for relationship attachments questions and answers

---
+++ src/domain/forum/enterprise/entities/answer-attachment.ts
@@ -0,0 +1,23 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface AnswerAttachmentProps {
+  answerId: UniqueEntityID
+  attachmentId: UniqueEntityID
+}
+
+export class AnswerAttachement extends Entity<AnswerAttachmentProps> {
+  get answerId() {
+    return this.props.answerId
+  }
+
+  get attachmentId() {
+    return this.props.attachmentId
+  }
+
+  static create(props: AnswerAttachmentProps, id?: UniqueEntityID) {
+    const answerAttachment = new AnswerAttachement(props, id)
+
+    return answerAttachment
+  }
+}
---
+++ src/domain/forum/enterprise/entities/attachment.ts
@@ -0,0 +1,25 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface AttachmentProps {
+  title: string
+  link: string
+  parentId: string
+  parentType: 'answer' | 'question'
+}
+
+export class Attachment extends Entity<AttachmentProps> {
+  get title() {
+    return this.props.title
+  }
+
+  get link() {
+    return this.props.link
+  }
+
+  static create(props: AttachmentProps, id?: UniqueEntityID) {
+    const attachment = new Attachment(props, id)
+
+    return attachment
+  }
+}
