e5196067584039ac8bd6318bf5df4ac41fe2dbab
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-04 17:18:23 -0300

    test: create a question with attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { InMemoryQuestionRepository } from 'test/repositories/in-memory-questions-respository'
 import { CreateQuestionUseCase } from './create-question'
 
@@ -10,14 +11,20 @@ describe('Create Questions', () => {
     sut = new CreateQuestionUseCase(inMemoryQuestionRepository)
   })
 
-  it('should be able to create a new question', async () => {
+  it('should be able to create a new question with attachemnts', async () => {
     const result = await sut.execute({
       authorId: '1',
       title: 'New question',
       content: 'Question content',
+      attachmentsIds: ['1', '2'],
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
   })
 })
