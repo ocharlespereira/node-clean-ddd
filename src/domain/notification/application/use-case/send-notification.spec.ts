40f6c629ee2b2528ea8c9512ac5872a5f6f7ffc0
CharlesPereira <charlesti.ptu@gmail.com>
2024-02-06 20:40:24 -0300

    test: ensure create a send notification

---
+++ src/domain/notification/application/use-case/send-notification.spec.ts
@@ -0,0 +1,26 @@
+import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
+import { SendNotificationUseCase } from './send-notification'
+
+let inMemoryNotificationsRepository: InMemoryNotificationsRepository
+let sut: SendNotificationUseCase
+
+describe('Send Notification', () => {
+  beforeEach(() => {
+    inMemoryNotificationsRepository = new InMemoryNotificationsRepository()
+
+    sut = new SendNotificationUseCase(inMemoryNotificationsRepository)
+  })
+
+  it('should be able to send a notification', async () => {
+    const result = await sut.execute({
+      recipientId: '1',
+      title: 'New notification',
+      content: 'Notification content',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryNotificationsRepository.items[0]).toEqual(
+      result.value?.notification
+    )
+  })
+})
